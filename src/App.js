d3d1c1d3ffacef33c22838c0fd61bf7b451e07df
Hailey Moon <82325205+Hailey-moon@users.noreply.github.com>
2022-08-11 17:26:28 -0700

    created work experiences and also changed from gotham to monsterrat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './style/App.css';
 import Header from './components/Header';
+import Experience from './components/Experience'
 
 function App() {
   return (
     <div>
       <Header />
+      <Experience
+        company="Cashmate"
+        description="An iOS budgeting app built to promote fiscal responsibility by motivating users through social engagement."
+        image="./static/cashmate-mockup.png"
+      />
+      <Experience
+        company="BostonHacks 2021"
+        description="One of the biggest student-run hackathons at Boston University, hosting over 260 students per year in the New England area."
+        image="./static/bhacks-mockup.png"
+      />
+      <Experience
+        company="Spark!"
+        description="Graphic design internship at Boston University's leading technology innovation organization."
+        image="./static/spark-mockup.png"
+      />
     </div>
   );
 }
